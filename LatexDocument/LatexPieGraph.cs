fff8b7104176d7d36a728dcda6d78dd8ed337cf5
salekin01 <salekin01@outlook.com>
2021-04-19 11:20:06 +0200

    Remove the LatexDocument.dll and add that as a class library

---
+++ LatexDocument/LatexPieGraph.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LatexDocument
+{
+    public class LatexPieGraph
+    {
+        List<LatexGraphValue> values;
+
+        /// <summary>
+        /// Specify if the graph should be centered
+        /// </summary>
+        public bool Centered { get; set; }
+
+        /// <summary>
+        /// Instantiates a new LatexPieGraph object
+        /// </summary>
+        /// <param name="Values">Values to be added in the graph</param>
+        public LatexPieGraph(List<LatexGraphValue> Values, bool Centered = true)
+        {
+            values = new List<LatexGraphValue>();
+            values.AddRange(Values);
+            this.Centered = Centered;
+        }
+
+        /// <summary>
+        /// Values to be added in the graph
+        /// </summary>
+        public List<LatexGraphValue> Values
+        {
+            get
+            {
+                return values;
+            }
+        }
+    }
+}
---
+++ LatexDocument/LatexTextTitle.cs
@@ -0,0 +1,36 @@
+﻿namespace LatexDocument
+{
+    public class LatexTextTitle
+    {
+        /// <summary>
+        /// Font of the title
+        /// </summary>
+        public LatexFont Font { get; set; }
+
+        /// <summary>
+        /// Text of the title to be added in the document
+        /// </summary>
+        public string Text { get; private set; }
+
+        /// <summary>
+        /// Instantiates a new LatexTextTitle object
+        /// </summary>
+        /// <param name="Text">Text of the title to be added in the document</param>
+        public LatexTextTitle(string Text)
+        {
+            this.Text = Text;
+            Font = new LatexFont();
+        }
+
+        /// <summary>
+        /// Instantiates a new LatexTextTitle object
+        /// </summary>
+        /// <param name="Text">Text of the title to be added in the document</param>
+        /// <param name="Font">Font of the title</param>
+        public LatexTextTitle(string Text, LatexFont Font)
+        {
+            this.Text = Text;
+            this.Font = Font;
+        }
+    }
+}
